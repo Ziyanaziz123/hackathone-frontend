fbe073f993006f82a0abb279735cf5b37a6385f6
ziyanaziz <ziyanazizz23@gmail.com>
2025-01-27 11:25:16 +0500

    ass code

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const url = "https://b-11-backend.vercel.app/api/"
+export const url = "https://backend-hackathone-git-main-ziyans-projects-27e4108f.vercel.app/api/"
 // export const url = "http://localhost:5000/api/"
